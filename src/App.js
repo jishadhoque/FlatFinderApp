9f22b72cab451f9e7aee3cf99025c0fd49e25706
Yaseen B <yaseenb@Yaseens-Mackbook-Air.local>
2025-04-09 19:15:27 +0100

    add listing now functional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 // Pages
-import HomePage from "./pages/homepage";
-import ListingsPage from "./pages/ListingsPage";
-import LoginPage from "./pages/login";
-import SignupPage from "./pages/signup";
-import ViewListingPage from "./pages/ViewListingPage";
-import InboxPage from "./pages/InboxPage" 
-// import AddListingPage from "./pages/addListingsPage";
+import HomePage from "./pages/homepage"
+import ListingsPage from "./pages/ListingsPage"
+import LoginPage from "./pages/login"
+import SignupPage from "./pages/signup"
+import ViewListingPage from "./pages/ViewListingPage"
+import InboxPage from "./pages/InboxPage"
+import AddListingPage from "./pages/AddListingsPage"
 
 function App() {
   return (
@@ -24,19 +20,19 @@ function App() {
         <Route path="/listings" element={<ListingsPage />} />
 
         <Route path="/signup" element={<SignupPage />} />
-        
+
         <Route path="/listing/:id" element={<ViewListingPage />} />
 
         {/* Inbox page */}
         <Route path="/inbox" element={<InboxPage />} />
 
-      
+        <Route path="/add-listing" element={<AddListingPage />} />
+
         {/* Login page */}
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
-
+export default App
